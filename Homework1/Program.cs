32d3825e22e928e54d81a9011a14b548c6ab2389
Aysylu23 <aysbad25@gmail.com>
2023-10-17 18:34:11 +0500

    Add homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Random randomizer = new Random();
+int minNumber = 100;
+int maxNumber = 999;
+
+int randomNumber = randomizer.Next(minNumber, maxNumber + 1);
+string randomNumberString = randomNumber.ToString();
+string resultString = $"{randomNumberString[1]}";
+int result = int.Parse(resultString);
+
+Console.WriteLine($"{randomNumber} -> {result}");
