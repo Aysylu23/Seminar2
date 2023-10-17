50c7d41e1c56e9c28ec75115dc742cf58cab26fd
Aysylu23 <aysbad25@gmail.com>
2023-10-17 22:58:13 +0500

    add changes

---
+++ Homework2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.WriteLine ("Введите число:");
+string str = Console.ReadLine();
+int number = Convert.ToInt32(str);
+
+
+Random randomizer = new Random();
+int minNumber = 100;
+int maxNumber = 999;
+
+int randomNumber = randomizer.Next(minNumber, maxNumber + 1);
+string randomNumberString = randomNumber.ToString();
+string resultString = $"{randomNumberString[0]}{randomNumberString[2]}";
+int result = int.Parse(resultString);
+
+Console.WriteLine($"{randomNumber} -> {result}");
+
